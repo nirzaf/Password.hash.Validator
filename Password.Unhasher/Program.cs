a1752d94ed3b4cd8fc24225df3b358a85c30176e
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-03-09 20:30:21 +0530

    VerifyHashedPassword

--- Password.Unhasher/Program.cs
+++ Password.Unhasher/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
-
 var ph = new PasswordHasher<User>();
 
 User user = new User
@@ -13,16 +12,6 @@ var hash = ph.HashPassword(user, "password");
 
 Console.WriteLine("Hash is " + hash);
 
-
 var isHashValid = ph.VerifyHashedPassword (user, "AQAAAAEAACcQAAAAEFlIiKvcyf6av+gCgUMIEEUbxyzrIOdp4bNk1x7fu2qnNRbprAdF5b2uPO0JTNOEXA==", "password");
 
-
-
 Console.WriteLine("Hash Validation is " + isHashValid);
-
-
-public class User
-{
-    public string Username { get; set; }
-    public string Password { get; set; }
-}
---
+++ Password.Unhasher/User.cs
@@ -0,0 +1,5 @@
+﻿public class User
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+}
