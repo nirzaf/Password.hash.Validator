b28ae76829d27403b147435e04dcc189028f9eb6
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-03-09 20:45:00 +0530

    //Console.WriteLine("Hash Validation is " + isHashValid);

--- Password.Unhasher/Program.cs
+++ Password.Unhasher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using static System.Console;
 
 var ph = new PasswordHasher<User>();
 
@@ -10,8 +11,19 @@ User user = new User
 
 var hash = ph.HashPassword(user, "password");
 
-Console.WriteLine("Hash is " + hash);
+WriteLine("Hash is " + hash);
 
 var isHashValid = ph.VerifyHashedPassword (user, "AQAAAAEAACcQAAAAEFlIiKvcyf6av+gCgUMIEEUbxyzrIOdp4bNk1x7fu2qnNRbprAdF5b2uPO0JTNOEXA==", "password");
+WriteLine("Hash Validation is " + isHashValid);
+for (int i = 0; i <= 100000000; i++)
+{
+    var pw = ph.VerifyHashedPassword(user, "ADwElLaoEJAKjc9QoNj57I/QtvPMrfxdTr7vO175aQ+b5Y1bJgXL4RSsjpBcmN6QZw==", i.ToString());
+    if (pw == PasswordVerificationResult.Success)
+    {
+        WriteLine(i.ToString());
+        break;
+    }
+    if(i== 100000000) WriteLine("Password did not match");
+}
 
-Console.WriteLine("Hash Validation is " + isHashValid);
+
